284d9a22c913389f05cea4635982440e463100a1
Dmytriivasylenko <160059389+Dmytriivasylenko@users.noreply.github.com>
2025-04-08 22:54:37 +0200

    Update celery.py

--- fleet/celery.py
+++ fleet/celery.py
@@ -2,13 +2,11 @@ from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 
-# Налаштування Django для використання Celery
+# Django settings for using Celery
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fleet')
-
 
 app = Celery('fleet')
 
-# Налаштування Celery
+# Celery configuration
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
-
